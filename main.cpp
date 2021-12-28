ead77028bcbcbe4811721c6fc6e8dedf402fd0d3
elthomaso <GueutalThomas@outlook.com>
2021-12-28 23:41:59 +0100

    sfml window test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <memory>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void windowTest() {
+
+    // TODO set window VSync to true
+    //  cap framerate at 60
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+}
+
+int main()
+{
+
+    // TODO
+    // TODO
+    //windowTest();
+    // TODO
+    // TODO
+
     return 0;
 }
