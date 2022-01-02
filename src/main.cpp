4cf10914f3ff896d4af7999d008f7ea5e821351a
elthomaso <GueutalThomas@outlook.com>
2022-01-03 00:54:53 +0100

    atomary commit: cluttered main with test of current functionality. Controls: arrows keys or 'zqsd' for movement, 'e' to invert requireCollision, which starts at false (read CollisionShape::determinePushback() documentation for more info) and 'a' to perform push back (essentially undo current collision, where the purple rect is static). The white line indicates the current 'movement direction' based on which the push back of the brown rect will occur.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <memory>
 
-#include "Utility/Stopwatch.h"
+#include "Entity/Player.h"
+#include "Collision/CollisionObject.h"
 
 // TODO  to modify difficulty, modify standard deviation of the normal distribution of Random???
 //  ==> If we normalize the result of random to the rang [0, 1], and we multiply default
@@ -20,20 +21,185 @@ void windowTest()
         // TODO set window VSync to true
         //  cap framerate at 60
 
-        sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+        sf::RenderWindow window(sf::VideoMode(600, 800), "SFML works!");
+        window.setFramerateLimit(60);
+
         sf::CircleShape shape(100.f);
+        //shape.setOrigin(50.f, 50.f);
+        //shape.setPosition(300 - 50.f, 400 - 50.f);
+        shape.setPosition(300, 400);
         shape.setFillColor(sf::Color::Green);
 
-        while (window.isOpen()) {
-                sf::Event event{};
-                while (window.pollEvent(event)) {
-                        if (event.type == sf::Event::Closed)
-                                window.close();
-                }
+        sf::ConvexShape quadI;
+        quadI.setPointCount(4);
+        quadI.setPoint(0, sf::Vector2f(0,0));
+        quadI.setPoint(1, sf::Vector2f(300,0));
+        quadI.setPoint(2, sf::Vector2f(300,400));
+        quadI.setPoint(3, sf::Vector2f(0,400));
+        quadI.setFillColor(sf::Color::Blue);
+        sf::ConvexShape quadII;
+        quadII.setPointCount(4);
+        quadII.setPoint(0, sf::Vector2f(300,0));
+        quadII.setPoint(1, sf::Vector2f(600,0));
+        quadII.setPoint(2, sf::Vector2f(600,400));
+        quadII.setPoint(3, sf::Vector2f(300,400));
+        quadII.setFillColor(sf::Color::Cyan);
+        sf::ConvexShape quadIII;
+        quadIII.setPointCount(4);
+        quadIII.setPoint(0, sf::Vector2f(300,400));
+        quadIII.setPoint(1, sf::Vector2f(600,400));
+        quadIII.setPoint(2, sf::Vector2f(600,800));
+        quadIII.setPoint(3, sf::Vector2f(300,800));
+        quadIII.setFillColor(sf::Color::Magenta);
+        sf::ConvexShape quadIV;
+        quadIV.setPointCount(4);
+        quadIV.setPoint(0, sf::Vector2f(0,400));
+        quadIV.setPoint(1, sf::Vector2f(300,400));
+        quadIV.setPoint(2, sf::Vector2f(300,800));
+        quadIV.setPoint(3, sf::Vector2f(0,800));
+        quadIV.setFillColor(sf::Color::Yellow);
 
-                window.clear();
-                window.draw(shape);
-                window.display();
+
+        Player* player = new Player(0, 0);
+
+        Rect r1 = {{{100.0, 100.0}, {200.0, 100.0},
+                    {200.0, 200.0}, {100.0, 200.0}}};
+        std::pair<double, double> c1 = CollisionObject::determineAbsoluteCenterOfMass(r1);
+        CollisionObject* cObj1 = new CollisionObject(
+                c1.first, c1.second,r1);
+
+        Rect r2 = {{{150.0, 150.0}, {250.0, 150.0},
+                    {250.0, 250.0}, {150.0, 250.0}}};
+        std::pair<double, double> c2 = CollisionObject::determineAbsoluteCenterOfMass(r2);
+        CollisionObject* cObj2 = new CollisionObject(
+                c2.first, c2.second,r2);
+
+        //cObj2->move(c2.first + 70, c2.second);
+
+        uint32_t ctr = 0;
+        int moveAmount = 5;
+        std::pair<double, double> moveDir{0, 0};
+        bool requireCollision = true;
+
+
+        while (window.isOpen()) {
+            sf::Event event{};
+            while (window.pollEvent(event)) {
+                    if (event.type == sf::Event::Closed)
+                            window.close();
+                    if (event.type == sf::Event::KeyPressed) {
+                        double dirX = 0.0;
+                        double dirY = 0.0;
+                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) ||
+                        sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+                            dirX += - moveAmount;
+                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) ||
+                        sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                            dirX += moveAmount;
+                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) ||
+                        sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                            dirY += - moveAmount;
+                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) ||
+                        sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+                            dirY += moveAmount;
+                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                            auto pushback = cObj2->determinePushback(moveDir, *cObj1, 1.0, requireCollision);
+                            cObj2->move(cObj2->getPosition().first + pushback.first,
+                                        cObj2->getPosition().second + pushback.second);
+                            std::cout << "(" << cObj2->getPosition().first << ", " << cObj2->getPosition().second << ")" << std::endl;
+                        } else {
+                            auto loc = cObj2->getPosition();
+                            cObj2->move(loc.first + dirX, loc.second + dirY);
+                            moveDir.first += dirX != 0 && dirX != moveDir.first ? dirX : 0;
+                            moveDir.second += dirY != 0 && dirY != moveDir.second ? dirY : 0;
+                        }
+                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+                            requireCollision = ! requireCollision;
+
+                    }
+            }
+
+            window.clear();
+
+
+            sf::ConvexShape rect1;
+            rect1.setPointCount(4);
+            Rect s1 = cObj1->getCollisionShape();
+            for (unsigned int i = 0; i < 4; ++i)
+                rect1.setPoint(i, sf::Vector2f(s1.at(i).first, s1.at(i).second));
+            rect1.setFillColor(sf::Color(150, 50, 250));
+
+            sf::CircleShape circle1(20);
+            // TODO    the origin is the center of mass of the shape???
+            circle1.setOrigin(circle1.getRadius(), circle1.getRadius());
+            circle1.setPosition(cObj1->getPosition().first, cObj1->getPosition().second);
+            circle1.setFillColor(sf::Color(255, 0, 0));
+
+
+            sf::ConvexShape rect2;
+            rect2.setPointCount(4);
+            Rect s2 = cObj2->getCollisionShape();
+            for (unsigned int i = 0; i < 4; ++i)
+                rect2.setPoint(i, sf::Vector2f(s2.at(i).first, s2.at(i).second));
+            rect2.setFillColor(sf::Color(150, 100, 150));
+
+            sf::CircleShape circle2(20);
+            circle2.setOrigin(circle2.getRadius(), circle2.getRadius());
+            circle2.setPosition(cObj2->getPosition().first, cObj2->getPosition().second);
+            circle2.setFillColor(sf::Color(255, 0, 0));
+
+
+            sf::CircleShape circle3(20);
+            circle3.setOrigin(circle3.getRadius(), circle3.getRadius());
+            circle3.setPosition(300.0, 400.0);
+            circle3.setFillColor(sf::Color(255, 0, 0));
+
+            // TODO  draw a line with length moveDir and some thickness
+            auto lineStart = CollisionObject::determineAbsoluteCenterOfMass(s2);
+            sf::Vertex line[] =
+                    {
+                            sf::Vertex(sf::Vector2f(lineStart.first, lineStart.second)),
+                            sf::Vertex(sf::Vector2f(lineStart.first + moveDir.first*8.0,
+                                                    lineStart.second + moveDir.second*8.0))
+                    };
+            sf::CircleShape arrowHead(5);
+            arrowHead.setOrigin(arrowHead.getRadius(), arrowHead.getRadius());
+            arrowHead.setPosition(lineStart.first + moveDir.first*8.0,lineStart.second + moveDir.second*8.0);
+
+            if (cObj1->checkCollision(*cObj2)) {
+                //std::cout << "coll" << std::endl;
+                rect1.setOutlineThickness(10);
+                rect1.setOutlineColor(sf::Color(250, 150, 100));
+            } else {
+                bool res = cObj1->checkCollision(*cObj2);
+                //std::cout << "no coll" << std::endl;
+            }
+
+            // TODO  add a rotate function to CollisionObject
+            // TODO  test the collision tests by moving and rotating the collision shapes.
+            //  The coloring of the border is already handled to show the collision
+
+            window.draw(quadI);
+            window.draw(quadII);
+            window.draw(quadIII);
+            window.draw(quadIV);
+
+            shape.rotate(1);
+            window.draw(shape);
+
+            window.draw(rect1);
+            window.draw(circle1);
+
+            window.draw(rect2);
+            window.draw(circle2);
+
+            window.draw(line, 2, sf::Lines);
+            window.draw(arrowHead);
+
+            window.draw(circle3);
+
+
+            window.display();
         }
 }
 
@@ -43,7 +209,7 @@ int main()
 
         // TODO
         // TODO
-        // windowTest();
+        windowTest();
         // TODO
         // TODO
 
