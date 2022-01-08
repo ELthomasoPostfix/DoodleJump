13a1abbd1df6d097580164db3ac038f79837bcf3
elthomaso <GueutalThomas@outlook.com>
2022-01-08 14:48:06 +0100

    ++ Updated CMakeLists.txt ++ -- added missing files:   ** Jetpack.cpp, Jetpack.h   ** Spring.cpp

---
+++ src/Game/GameObject/Entity/Bonus/Jetpack.h
@@ -0,0 +1,18 @@
+//
+// Created by Thomas Gueutal on 13/11/2021.
+//
+
+#ifndef DOODLEJUMP_JETPACK_H
+#define DOODLEJUMP_JETPACK_H
+
+#include "Bonus.h"
+
+
+class Jetpack : Bonus {
+    public:
+        ~Jetpack() override = default;
+
+};
+
+
+#endif //DOODLEJUMP_JETPACK_H
---
+++ src/Game/GameObject/Entity/Bonus/Spring.h
@@ -0,0 +1,18 @@
+//
+// Created by Thomas Gueutal on 13/11/2021.
+//
+
+#ifndef DOODLEJUMP_SPRING_H
+#define DOODLEJUMP_SPRING_H
+
+#include "Bonus.h"
+
+
+class Spring : Bonus {
+    public:
+        ~Spring() override = default;
+
+};
+
+
+#endif //DOODLEJUMP_SPRING_H
