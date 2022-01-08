584279c2b95bc7125c48e26fbd1a99caead4deda
elthomaso <GueutalThomas@outlook.com>
2022-01-08 14:33:23 +0100

    ++ Updated CMakeLists.txt ++ -- added missing files:   ** Bonus.cpp, Bonus.h   ** BGTile.cpp, BGTile.h

---
+++ src/Game/GameObject/Entity/BGTile/BGTile.h
@@ -0,0 +1,17 @@
+//
+// Created by Thomas Gueutal on 13/11/2021.
+//
+
+#ifndef DOODLEJUMP_BGTILE_H
+#define DOODLEJUMP_BGTILE_H
+
+#include "../Entity.h"
+
+
+class BGTile : Entity {
+    public:
+        ~BGTile() override = default;
+};
+
+
+#endif //DOODLEJUMP_BGTILE_H
---
+++ src/Game/GameObject/Entity/Bonus/Bonus.h
@@ -0,0 +1,17 @@
+//
+// Created by Thomas Gueutal on 13/11/2021.
+//
+
+#ifndef DOODLEJUMP_BONUS_H
+#define DOODLEJUMP_BONUS_H
+
+#include "../Entity.h"
+
+
+class Bonus : Entity {
+    public:
+        ~Bonus() override = default;
+};
+
+
+#endif //DOODLEJUMP_BONUS_H
