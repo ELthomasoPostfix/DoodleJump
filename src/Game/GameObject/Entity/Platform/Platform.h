f375027bc5d891cd8fb0b3177eda887a8feb87db
elthomaso <GueutalThomas@outlook.com>
2022-01-08 14:30:11 +0100

    ++ Updated CMakeLists.txt ++

---
+++ src/Game/GameObject/Entity/Platform/Platform.h
@@ -0,0 +1,17 @@
+//
+// Created by Thomas Gueutal on 13/11/2021.
+//
+
+#ifndef DOODLEJUMP_PLATFORM_H
+#define DOODLEJUMP_PLATFORM_H
+
+#include "../Entity.h"
+
+
+class Platform : Entity {
+    public:
+        ~Platform() override = default;
+};
+
+
+#endif //DOODLEJUMP_PLATFORM_H
