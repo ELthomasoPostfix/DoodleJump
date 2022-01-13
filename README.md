665236e4f23f5b07231cb773842e901c4e62f306
elthomaso <GueutalThomas@outlook.com>
2022-01-13 21:30:30 +0100

    ++ exception ++ -- Added resource loading error message   ==> executable must be run from a directory containing the Resources folder

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DoodleJump
 A project for the course Advanced Programming for the University of Antwerp during the academic year of 2021-2022.
+
+# How to run
+The file resources.h, compiled as a part of the DoodleView static library, contains the paths of the sprites used in
+creating the different entities. As this file is used in compilation, it defines from where the executable should be run
+at compile time. This location cannot currently be redefined without recompiling (at least I do not know how).
+The unmodified resources.h file as found on this github repository requires the executable to be run from a folder
+that contains the Resources directory found on this github page. Otherwise, an error reporting an incorrect file path
+will be printed.
--- src/Game.cpp
+++ src/Game.cpp
@@ -12,11 +12,16 @@
 Game::Game(const unsigned int windowWidth, const unsigned int windowHeight)
     : _windowManager(std::make_unique<SFMLWindowManager>(windowWidth, windowHeight)) {
 
-    _windowManager->textureManager->load(PLAYER_TEXTURE_ID, PLAYER_TEXTURE_PATH);
-    _windowManager->textureManager->load(SPRING_TEXTURE_ID, SPRING_TEXTURE_PATH);
-    _windowManager->textureManager->load(JETPACK_TEXTURE_ID, JETPACK_TEXTURE_PATH);
-    _windowManager->textureManager->load(BGTILE_TEXTURE_ID, BGTILE_TEXTURE_PATH);
-    _windowManager->fontManager->load(ARIAL_FONT_ID,ARIAL_FONT_PATH);
+    try {
+
+        _windowManager->textureManager->load(PLAYER_TEXTURE_ID, PLAYER_TEXTURE_PATH);
+        _windowManager->textureManager->load(SPRING_TEXTURE_ID, SPRING_TEXTURE_PATH);
+        _windowManager->textureManager->load(JETPACK_TEXTURE_ID, JETPACK_TEXTURE_PATH);
+        _windowManager->textureManager->load(BGTILE_TEXTURE_ID, BGTILE_TEXTURE_PATH);
+        _windowManager->fontManager->load(ARIAL_FONT_ID, ARIAL_FONT_PATH);
+    } catch (std::runtime_error& e) {
+        std::cout << "Game could not load resource files (Executable must be run from directory containing Resources folder)\n  what():   " << e.what() << std::endl;
+    }
 
     _texturesInfo.playerTextureDims  = _windowManager->getTextureDimensions(PLAYER_TEXTURE_ID);
     _texturesInfo.springTextureDims  = _windowManager->getTextureDimensions(SPRING_TEXTURE_ID);
